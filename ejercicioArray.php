bb3dcebd296e364b76dbcdc3874c9e92091e4d38
AstridTania <90683662+AstridTania@users.noreply.github.com>
2021-10-06 12:32:43 +0200

    Add files via upload

---
+++ ejercicioArray.php
@@ -0,0 +1,52 @@
+<?php
+  $matriz_empresas=['Empresa1'  => ['dato11'=>11,'dato12'=>12,'dato13'=>13,'dato14'=>14,'dato15'=>15,'dato16'=>16,'dato17'=>17,'dato18'=>18,'dato19'=>19],
+                  'Emperesa2' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa3' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa4' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa5' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa6' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa7' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa8' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa9' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa10' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa11' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa12' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa13' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa14' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa15' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa16' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa17' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa18' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa19' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa20' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa21' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa22' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa23' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa24' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa25' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa26' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa27' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa28' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa29' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa30' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa31' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa32' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa33' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa34' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa35' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa23' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa23' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa23' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19],
+				  'Emperesa23' => ['dato21'=>11,'dato22'=>12,'dato23'=>13,'dato23'=>14,'dato23'=>15,'dato23'=>16,'dato23'=>17,'dato23'=>18,'dato23'=>19]];
+				  
+				  
+                 
+
+  echo 'empresas<br>'; 
+ echo('<pre>');
+var_dump($matriz_empresas);
+echo('</pre>'); 
+  
+ 
+ echo  $matriz_empresas['Emperesa12']['dato23'].'<br>';
+?>
